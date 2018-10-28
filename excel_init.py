4af4babd5e663fd4082edc67b9bf57a3f8e36870
eric <sunzenghui@shphschool.com>
2018-10-28 19:51:40 +0800

    prototype

---
+++ excel_init.py
@@ -0,0 +1,24 @@
+from openpyxl import Workbook
+
+def init():
+    tickers = ["AAPL", "GOOGL"]
+    wb = Workbook()
+    first_time = True
+
+    for ticker in tickers:
+
+        if first_time:
+            ws = wb.active
+            ws.title = ticker
+            first_time = False
+        else:
+            ws = wb.create_sheet(ticker)
+
+        title = [" ", "price", "pe_ratio", "volume", "avg_volume", "beta", "market_cap", "eps", "earning_date", "dividend_yield"]
+        ws.append(title)
+
+    print(wb.sheetnames)
+    wb.save('database.xlsx')
+
+
+init()
