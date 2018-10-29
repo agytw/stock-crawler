405eee6e6e36f3457ebbc4de874d1c43c50e3d3b
eric <sunzenghui@shphschool.com>
2018-10-29 10:51:25 +0800

    fix a bug on https_get added frequency getting

--- main.py
+++ main.py
@@ -1,50 +1,85 @@
 import get_data
 import basics
 from openpyxl import load_workbook
-from datetime import datetime
+from datetime import datetime, timedelta
 
 tickers = ['AAPL', "GOOGL"]
-wb = load_workbook('database.xlsx')
 
-for ticker in tickers:
-    # title = ["price", "pe_ratio", "volume", "avg_volume", "beta", "market_cap", "eps", "earning_date", "dividend_yield"]
-    value_inserted = []
-    time = datetime.now()
-    value_inserted.append(time.strftime('%Y-%m-%d %H:%M'))
+one_Day_date = datetime.now()
+three_Day_date = datetime.now()
+half_Hour_date = datetime.now()
 
-    decoded_item = basics.decode(ticker)
-    table = get_data.generate_info_table(decoded_item)
 
-    price = get_data.get_price(decoded_item)
-    value_inserted.append(price)
+def main_action(one_day_date, three_day_date):
 
-    pe_ratio = get_data.get_info("pe_ratio", table)
-    value_inserted.append(pe_ratio)
+    wb = load_workbook('database.xlsx')
+    now = datetime.now().strftime('%Y-%m-%d %H:%M')
+    str_one = one_day_date.strftime('%Y-%m-%d %H:%M')
+    str_three = three_day_date.strftime('%Y-%m-%d %H:%M')
+    reached_one_day = now == str_one
+    reached_three_day = now == str_three
+    print(reached_one_day)
+    print(reached_three_day)
 
-    volume = get_data.get_info("volume", table)
-    value_inserted.append(volume)
+    for ticker in tickers:
+        # ["price", "pe_ratio", "volume", "avg_volume", "beta", "market_cap", "eps", "earning_date", "dividend_yield"]
+        print("search for " + ticker)
+        value_inserted = []
+        time = datetime.now()
+        value_inserted.append(time.strftime('%Y-%m-%d %H:%M'))
 
-    avg_volume = get_data.get_info("avg_volume", table)
-    value_inserted.append(avg_volume)
+        decoded_item = basics.decode(ticker)
+        table = get_data.generate_info_table(decoded_item)
 
-    beta = get_data.get_info("beta", table)
-    value_inserted.append(beta)
+        price = get_data.get_price(decoded_item)
+        value_inserted.append(price)
 
-    market_cap = get_data.get_info("market_cap", table)
-    value_inserted.append(market_cap)
+        if reached_one_day:
 
-    eps = get_data.get_info("eps", table)
-    value_inserted.append(eps)
+            pe_ratio = get_data.get_info("pe_ratio", table)
+            value_inserted.append(pe_ratio)
 
-    earning_date = get_data.get_info("earning_date", table)
-    value_inserted.append(earning_date)
+            volume = get_data.get_info("volume", table)
+            value_inserted.append(volume)
 
-    dividend_yield = get_data.get_info("dividend_yield", table)
-    value_inserted.append(dividend_yield)
+            avg_volume = get_data.get_info("avg_volume", table)
+            value_inserted.append(avg_volume)
 
-    wb[ticker].append(value_inserted)
+            beta = get_data.get_info("beta", table)
+            value_inserted.append(beta)
+
+            market_cap = get_data.get_info("market_cap", table)
+            value_inserted.append(market_cap)
+
+            eps = get_data.get_info("eps", table)
+            value_inserted.append(eps)
+
+            one_day_date = one_day_date + timedelta(days=1)
+
+        if reached_three_day:
+
+            earning_date = get_data.get_info("earning_date", table)
+            value_inserted.append(earning_date)
+
+            dividend_yield = get_data.get_info("dividend_yield", table)
+            value_inserted.append(dividend_yield)
+
+            three_day_date = three_day_date + timedelta(days=3)
+
+        wb[ticker].append(value_inserted)
+        print(value_inserted)
+
     wb.save('database.xlsx')
+    print('saved')
     # info = news.get_news(ticker)
-    print(value_inserted)
+
+    return one_day_date, three_day_date
 
 
+while True:
+    current = datetime.now().strftime('%Y-%m-%d %H:%M')
+    str_half = half_Hour_date.strftime('%Y-%m-%d %H:%M')
+    if current == str_half:
+        print(current)
+        one_Day_date, three_Day_date = main_action(one_Day_date, three_Day_date)
+        half_Hour_date = half_Hour_date + timedelta(minutes=30)
