4a556b64cb6dd991ce15282dd86500cf27160bbd
eric <sunzenghui@shphschool.com>
2018-10-30 22:18:54 +0800

    static file

--- static_file.py
+++ static_file.py
@@ -1,18 +1,37 @@
 import pickle
 import os
+import datetime
 
 current_path = os.path.dirname(__file__)
 name = "variables.txt"
 
 
 def initialize(fname):
-    os.mknod(current_path + "/datas/" + fname)
+    file = open("datas/" + name, "wb")
     data = {
-        "half_hour_date" : 0,
-
-
+        "half_hour_date": datetime.datetime.now(),
+        "one_day_date": datetime.datetime.now(),
+        "three_day_date": datetime.datetime.now(),
+        "finished": [],
+        "tickers": []
     }
+    print(data)
+    pickle.dump(data, file)
 
 
-def change_var(var, value):
-    file = open(current_path + "/datas/" + name, "rb+")
+def set_var(var, value):
+    file = open("/datas/" + name, "rb+")
+    data = pickle.load(file)
+    data[var] = value
+    print(data)
+    pickle.dump(data, file)
+
+
+def get_var(var):
+    file = open("/datas/" + name, "rb+")
+    data = pickle.load(file)
+    return data[var]
+
+
+if __name__ == '__main__':
+    initialize(name)
