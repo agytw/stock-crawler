06a1808ffdeaac0f1f497bd522e5cbf02acf8f78
shuye02 <shuye02@outlook.com>
2018-10-31 20:33:45 +0800

    [Minor] Bug fix

--- crawler/main.py
+++ crawler/main.py
@@ -7,7 +7,7 @@ import config
 
 ## Check if database.xlsx exists
 from pathlib import Path
-my_file = Path(datas/database.xlsx)
+my_file = Path('datas/database.xlsx')
 if my_file.is_file():
     print('database.xlsx exists, using it.')
 else:
@@ -24,7 +24,7 @@ half_Hour_date = datetime.now()  # 启动时先进行一次所有搜索
 
 def main_action(one_day_date, three_day_date):
 
-    wb = load_workbook(datas/database.xlsx)
+    wb = load_workbook('datas/database.xlsx')
     now = datetime.now().strftime('%Y-%m-%d %H:%M')
     str_one = one_day_date.strftime('%Y-%m-%d %H:%M')  # 似乎不变成字符串形式不好抹掉秒后面的时间，不抹不好比
     str_three = three_day_date.strftime('%Y-%m-%d %H:%M')
