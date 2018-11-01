ef15a81eb93c8c77490a1accc5941c7fd1b78c0b
eric <sunzenghui@shphschool.com>
2018-11-01 17:59:28 +0800

    used sech to schedule events added retrying of the retrieval

--- crawler/main.py
+++ crawler/main.py
@@ -2,10 +2,14 @@ import get_data
 import basics
 from openpyxl import load_workbook
 from datetime import datetime, timedelta
-
+import sched, time
 import config
 
-## Check if database.xlsx exists
+
+
+schedule = sched.scheduler(time.time, time.sleep)
+
+# Check if database.xlsx exists
 from pathlib import Path
 my_file = Path('datas/database.xlsx')
 if my_file.is_file():
@@ -16,31 +20,58 @@ else:
     initialization.init()
     print('Auto generated')
 
-one_Day_date = datetime.now()
-three_Day_date = datetime.now()
-half_Hour_date = datetime.now()  # 启动时先进行一次所有搜索
+
+global reached_one_day, reached_three_day
+reached_one_day = False
+reached_three_day = False
+
+# 启动时先进行一次所有搜索
 # 整体思路是：半小时爬一次股价，再爬股价的时候检查时间有没有到 一天/三天，如果到了的话顺便分析一下剩下的
 
 
-def main_action(one_day_date, three_day_date):
+def set_cycle(times):  # 设定一个三天循环周期
 
+    for x in range(times):  # 三天提醒
+        schedule.enter(86400 * 3 * x, 1, set_one_date)
+
+    for x in range(3*times):  # 一天提醒
+        schedule.enter(86400*x, 2, set_three_date)
+
+    for x in range(times*24*2*3):  # 半小时提醒
+        schedule.enter(1800*x, 3, main_action)
+
+
+def set_one_date():
+    global reached_one_day
+    reached_one_day = True
+
+
+def set_three_date():
+    global reached_three_day
+    reached_three_day = True
+
+
+def main_action():
+    global reached_one_day, reached_three_day, decoded_item
     wb = load_workbook('datas/database.xlsx')
-    now = datetime.now().strftime('%Y-%m-%d %H:%M')
-    str_one = one_day_date.strftime('%Y-%m-%d %H:%M')  # 似乎不变成字符串形式不好抹掉秒后面的时间，不抹不好比
-    str_three = three_day_date.strftime('%Y-%m-%d %H:%M')
-    reached_one_day = now == str_one  # 是否到一天
-    reached_three_day = now == str_three  # 是否到三天
-    print(reached_one_day)
-    print(reached_three_day)
 
     for ticker in config.tickers:
         # ["price", "pe_ratio", "volume", "avg_volume", "beta", "market_cap", "eps", "earning_date", "dividend_yield"]
-        print("search for " + ticker)
+        print("download for " + ticker)
+        for retires in range(5):  # 重试
+            try:
+                global decoded_item
+                decoded_item = basics.decode(ticker)
+                break
+            except UnicodeDecodeError as e:
+                print('Network error, retrying')
+
+        if retires == 4:
+            raise("Network is Fking slow")
+
         value_inserted = []  # 看上面注释
         time = datetime.now()
         value_inserted.append(time.strftime('%Y-%m-%d %H:%M'))
-
-        decoded_item = basics.decode(ticker)
         table = get_data.generate_info_table(decoded_item)
 
         price = get_data.get_price(decoded_item)  # 半小时找price
@@ -66,7 +97,7 @@ def main_action(one_day_date, three_day_date):
             eps = get_data.get_info("eps", table)
             value_inserted.append(eps)
 
-            one_day_date = one_day_date + timedelta(days=1)
+            reached_one_day = False
 
         if reached_three_day:  # 到三天了
 
@@ -76,7 +107,7 @@ def main_action(one_day_date, three_day_date):
             dividend_yield = get_data.get_info("dividend_yield", table)
             value_inserted.append(dividend_yield)
 
-            three_day_date = three_day_date + timedelta(days=3)
+            reached_three_day = False
 
         wb[ticker].append(value_inserted)
         print(value_inserted)
@@ -84,13 +115,7 @@ def main_action(one_day_date, three_day_date):
         print('saved')
     # info = news.get_news(ticker)
 
-    return one_day_date, three_day_date
 
-
-while True:  # 一直执行
-    current = datetime.now().strftime('%Y-%m-%d %H:%M')
-    str_half = half_Hour_date.strftime('%Y-%m-%d %H:%M')
-    if current == str_half:
-        print(current)
-        one_Day_date, three_Day_date = main_action(one_Day_date, three_Day_date)
-        half_Hour_date = half_Hour_date + timedelta(minutes=30)
+if __name__ == '__main__':
+    set_cycle(5)  # 设定15天
+    schedule.run()
